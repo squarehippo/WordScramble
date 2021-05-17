f7a1d3c9e76ffe6c09da19aa9d4ce69de102b03c
Brian Wilson <squarehippobills@gmail.com>
2021-05-17 14:29:07 -0400

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Brian Wilson on 5/17/21.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
